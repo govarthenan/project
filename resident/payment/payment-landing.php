d57b5fcbddbfa4bf579ef33755042a312fbaaf6f
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-01-19 15:57:30 +0530

    Styled the payment landing

--- resident/payment/payment-landing.php
+++ resident/payment/payment-landing.php
@@ -11,8 +11,68 @@ include('sidenav.php');
   </head>
   <body>
     <div class="main-content">
-      <div class="payment">
-        
+      <div class="payment-container">
+        <div class="payment-diagrams">
+           <div class="payment-summary-graph">
+             <p>bsdfhbsdfbsd</p>
+           </div>
+           <div class="payment-calendar">
+
+           </div>
+        </div>
+      <div class="payment-details">
+      <div class="payment-history">
+      <div class="booking-history-heading">
+              <img src="payment-history.svg" class="payment-history-img" />
+              <span class="payment-history-title">Payment History</span>
+           </div>
+           <hr>
+              
+            <div class="booking-tiles-history">
+              <div class="booking-date-history"> 2024/01/20</div>
+              <div class="booking-time-history"> 8.00 a.m</div>
+              <div class="booking-type-history"> Fitness Center</div>
+              <div class="booking-status">
+                <p class="booking-status-completed">Completed</p>
+              </div>
+            </div>
+            <div class="booking-tiles-history">
+              <div class="booking-date-history"> 2024/01/23</div>
+              <div class="booking-time-history"> 8.00 a.m</div>
+              <div class="booking-type-history"> Fitness Center</div>
+              <div class="booking-status">
+                <p class="booking-status-completed">Completed</p>
+              </div>
+            </div>
+            
+            <div class="booking-tiles-history">
+              <div class="booking-date-history"> 2024/01/25</div>
+              <div class="booking-time-history"> 5.00 p.m</div>
+              <div class="booking-type-history"> Pool</div>
+              <div class="booking-status">
+                <p class="booking-status-completed">Completed</p>
+              </div>
+            </div> 
+          </div>
+         
+          <div class="up-coming-payments">
+          <div class="up-coming-payment-heading">
+              <img src="up_coming_payment.svg" class="up-coming-payment-img" />
+              <span class="up-coming-payment-title">Up Coming Payments</span>
+            </div>
+           <hr>
+            <div class="payment-tiles">
+              <div class="payment-date"> 2024/01/20</div>
+              <div class="payment-type"> Manintenace</div>
+            </div>
+            <div class="payment-tiles">
+              <div class="payment-date"> 2024/01/23</div>
+              <div class="payment-type">Maintenance</div>
+            </div>
+            
+           
+          </div>
+          </div>
       </div>
    </div>
 </body>
