e637d6a487a53cead75a79367eadbd9c7eca630e
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-01-24 14:35:38 +0530

    Create the notification part of the dashboard.

--- resident/dashboard/dashboard.php
+++ resident/dashboard/dashboard.php
@@ -14,7 +14,39 @@ include('sidenav.php');
       <div class="dashboard-container">
         <div class="first-row">
           <div class="announcment-container">
-
+            <div class="announcment-heading">
+              Announcment
+            </div>
+            
+            <div class="announcment-tile-container">
+               <div class="announcment-1">
+                  <div class="announcment">
+                    <div class="announcment-tile-heading">Monthly General meeting</div>
+                      <div class="announcment-details">Dear residents,<br>
+                         We are excited to announce our general meeting for the month of February.<br>
+                        <b>Date:2024.02.27</b><br>
+                        <b>Time:8.00 a.m</b><br>
+                        <b>Location:</b><br>Please make sure to attend and participate. Your presence is highly valued!<br>
+                         Thank you,<br>
+                         General manager.
+                      </div>
+                   </div>
+                  </div>
+               <div class="announcment-2">
+               <div class="announcment">
+                    <div class="announcment-tile-heading">Power Cut Announcement</div>
+                      <div class="announcment-details">Dear residents,<br>
+                      We would like to inform you about a scheduled power cut on the 4th of February.<br>
+                        <b>Date:2024.02.27</b><br>
+                        <b>Time:8.00 a.m</b><br>
+                        <b>Duration:6 hours</b><br>
+                        <b>Reason:Maitenance repair</b><br> We apologize for any inconvenience this may cause and appreciate your understanding.<br>
+                         Thank you,<br>
+                         Facility manager.
+                      </div>
+                   </div>
+               </div>
+            </div>
           </div>
           <div class="calendar">
             <div class="calendar-container">
@@ -52,19 +84,25 @@ include('sidenav.php');
   
         <div class="second-row">
           <div class="booking-reminder">
-
+            <img src="waiting-appointments-calendar.svg" class="booking-reminder-img">
+            <div class="reminder-heading">2024/01/20<br>Fitness Center</div>
           </div>
           <div class="event-reminder">
-
+             <img src="calendar-star.svg" class="event-reminder-img">
+             <div class="reminder-heading">2024/01/30<br>Monthly Meeting</div>
           </div>
           <div class="payment-reminder">
-
+            <img src="subscription-cashflow.svg" class="payment-reminder-img">
+            <div class="reminder-heading">2024/02/20<br>Maintenance Fees</div>
           </div>
-          <div class="private-files"></div>
+          <div class="private-files">
+           <img src="business-user-curriculum.svg" class="business-user-curriculum-img">
+           <div class="private-files-heading">Private files</div>
+          </div>
         </div>
         <div class="third-row">
           <div class="maitenance-payment-overview">
-
+            
           </div>
           <div class="utility-useage-overview">
 
