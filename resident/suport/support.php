d160934181a1407f13d1e139c02912783a083987
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-02 16:26:51 +0530

    Created the support page.

---
+++ resident/suport/support.php
@@ -0,0 +1,72 @@
+<?php
+include('sidenav.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="main-content">
+      <div class="support-container">
+        <div class="support-center-heading">
+          Support Center
+        </div>
+        <div class="support-intro">
+         Welcome to our Support Center for the Flat Management System. We are dedicated to providing you with the assistance you need to ensure a seamless and enjoyable experience with our platform. Whether you have questions, concerns, or are facing any issues, our team is here to help. Please take advantage of the resources provided below to find the assistance you require.</p>
+        </div>
+          <div class="support-center-content">
+           <div class="column-1">
+             <div class="support-sub-heading">Contact Information:-</div>
+             <ul>
+               <li ><b>Phone Number:</b><i> +1 (555) 123-4567</i></li>
+               <li ><b>Email:</b><i> support@castle.com</i></li>
+               <li><b>Assistance:</b> 070-1275832</i>
+               <li><b>Emergency Hotline:</b><i>+1 (555) 999-9999</i></li>
+            </ul>
+            <div class="support-sub-heading">Registration:-</div>
+              <div class="supoort-details">Each person who buys an apartment will get a user account of type "resident".
+                                           After validationg the identity of apartment buyer, registration of resident accounts is done by the receptionist at the receptionist desk.
+                                           Once the registration process completed, apartment owner will recieve an email of confirmation with his username and password.
+              </div>
+            <div class="support-sub-heading"> Common area Reservation:-</div>
+              <div class="supoort-details">Apartment owner can login in to the system using username and password recieved to his/her email.
+                                           After the login into the system for the first time, it is his/her responsibility to change the password interms of security concerns.
+              </div>
+              <div class="support-sub-heading">Maintenane and Technical Issue:-</div>
+                <div class="supoort-details">Apartment owner can login in to the system using username and password recieved to his/her email.
+                                           After the login into the system for the first time, it is his/her responsibility to change the password interms of security concerns.
+                </div>
+           </div>
+           <div class="column-2">
+           <div class="support-sub-heading">Technical Support:-</div>
+             <ul>
+              <li><b>Phone Number:</b><i>  +1 (555) 123-4567</i></li>
+              <li><b>Email:</b><i> techsupport@example.com</i></li>
+            </ul>
+            <div class="support-sub-heading">Login:-</div>
+             <div class="supoort-details">Apartment owner can login in to the system using username and password recieved to his/her email.
+                                          After the login into the system for the first time, it is his/her responsibility to change the password interms of security concerns.
+             </div>
+            <div class="support-sub-heading">Fitness Centre Reservation:-</div>
+              <div class="supoort-details">Apartment owner can login in to the system using username and password recieved to his/her email.
+                                           After the login into the system for the first time, it is his/her responsibility to change the password interms of security concerns.
+              </div>
+              <div class="support-sub-heading">Payments:-</div>
+                <div class="supoort-details">Apartment owner can login in to the system using username and password recieved to his/her email.
+                                           After the login into the system for the first time, it is his/her responsibility to change the password interms of security concerns.
+                </div>
+                <div class="support-sub-heading">Complaints:-</div>
+                <div class="supoort-details">Apartment owner can login in to the system using username and password recieved to his/her email.
+                                           After the login into the system for the first time, it is his/her responsibility to change the password interms of security concerns.
+                </div>
+           </div>
+           
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
