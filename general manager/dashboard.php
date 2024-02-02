5d5200e421bdf83f8568d4075ca30f67dfa4c5ed
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-02 16:25:37 +0530

    Created general manager dashboard.

---
+++ general manager/dashboard.php
@@ -0,0 +1,51 @@
+<?php
+include('sidenav.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+  <div class="main-content">
+    <div class="dashboard-container-general-manger">
+      <div class="first-column">
+        <div class="first-row">
+          <div class="box-1">
+
+          </div>
+          <div class="box-2">
+            
+          </div>
+          <div class="box-3">
+            
+          </div>
+        </div>
+        <div class="second-row">
+
+        </div>
+        <div class="third-row">
+          <div class="rectangle-1">
+
+          </div>
+          <div class="rectangle-2">
+
+          </div>
+        </div>
+      </div>
+      <div class="second-column">
+        <div class="column-box-1">
+
+        </div>
+        <div class="column-box-2">
+          
+        </div>
+      </div>
+   </div>
+  </div>
+</body>
+</html>
+  
