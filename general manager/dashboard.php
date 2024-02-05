1e50550420a13c43a47d0e1e351dcd188cfa9ae3
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-05 09:40:59 +0530

    styled the dashboard tiles.

--- general manager/dashboard.php
+++ general manager/dashboard.php
@@ -14,22 +14,46 @@ include('sidenav.php');
     <div class="dashboard-container-general-manger">
       <div class="first-column">
         <div class="first-row">
-          <div class="box-1">
-
+          <div class="no-of-flats" >
+            <div class="title-dashboard-tiles">Flats Overview</div>
+            <div class="content-info">
+              <div class="total-flats">
+                Total Flats: <span id="totalFlats">100</span>
+              </div>
+              <div class="sold-flats">
+               Sold Flats: <span id="soldFlats">70</span>
+              </div>
+              <div class="remaining-flats">
+              Remaining Flats: <span id="remainingFlats">30</span>
+              </div>
+            </div>
           </div>
-          <div class="box-2">
-            
+          <div class="total-residents">
+            <div class="title-dashboard-tiles">Gender Distribution</div>
+              <div class="content-info">
+               <div class="gender-distribution women">
+                 Women: <span id="womenCount">120</span>
+              </div>
+              <div class="gender-distribution men">
+                Men: <span id="menCount">150</span>
+              </div>
+            </div>
           </div>
-          <div class="box-3">
-            
+          <div class="no-of-staff">
+            <div class="title-dashboard-tiles">Staff Members</div>
+              <div class="content-info">
+                <div class="staff-count">
+                  Total Staff: <span id="staffCount">85</span>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
         <div class="second-row">
 
         </div>
         <div class="third-row">
           <div class="rectangle-1">
-
+          <canvas id="consumptionChart"></canvas>
           </div>
           <div class="rectangle-2">
 
@@ -46,6 +70,8 @@ include('sidenav.php');
       </div>
    </div>
   </div>
+  <script src="index.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 </html>
   
