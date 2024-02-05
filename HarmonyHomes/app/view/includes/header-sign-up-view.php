cb6674d4aca72ec95aebbcda13997ffd539ee737
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-05 12:49:43 +0530

    Included the sign up page.

---
+++ HarmonyHomes/app/view/includes/header-sign-up-view.php
@@ -0,0 +1,13 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link rel="stylesheet" href="../public/css/style.css" />
+        
+        <meta name="description" content="Free Web tutorials">
+         <meta name="keywords" content="HTML, CSS, JavaScript">
+         
+    </head>
+    <body>
+        <div style="min-width=350px;">
