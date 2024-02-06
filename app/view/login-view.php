dc2d88a286b733c2de034d4eba4f70c44b9ab714
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-06 20:43:53 +0530

    Finished the sign-up and sign-in styling.

--- app/view/login-view.php
+++ app/view/login-view.php
@@ -4,29 +4,28 @@ $this->view("includes/header");?>
     <div class="main-container-sign-in">
       <div class="sign-in-container">
         <div class="sign-in-image-container">
-         <img src="././public/assets/img/Mobile login-cuate.svg" class="sign-in-container-company-logo">
+         <img src="././public/assets/img/Computer login-bro.svg" class="sign-in-container-company-logo">
         </div>
         <div class="sign-in-form-container">
           <div class="login-form">
             <div class="company-name-sign-in">Harmony Homes!</div>
            <img src="logo.svg" class="logo-sign-in">
             <form action="/submit-login" method="post">
-              <div class="form-group">
+              <div class="sign-in-form-group">
                 <label for="username">Username:</label>
                 <input type="text" id="username" name="username" required>
               </div>
-              <div class="form-group">
+              <div class="sign-in-form-group">
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password" required>
               </div>
-              <div class="form-group forgot-password">
+              <div class="sign-in-form-group forgot-password">
               <a href="/forgot-password">Forgot Password?</a>
              </div>
               <button type="submit" class="btn">Login</button>
             </form>
           </div>
         </div>
-      </div> 
-       
+      </div>  
     </div>
     <?php $this->view("includes/footer"); ?>
---
+++ app/view/sign-up-view.php
@@ -0,0 +1,75 @@
+<?php
+$this->view("includes/header");?>
+
+<div class="main-container-sign-up">
+       <div class="company-name">
+           Harmony Homes! 
+        </div>
+       <div class="sign-up-img">
+         <img src="././public/assets/img/Sign up.svg" class="sign-up-image">
+        </div>
+      <div class="sign-up-container">
+        <div class="sign-up-message"> Sign up to become a member of our residence managment system....</div>
+          <form action="submitForm.php" method="post" enctype="multipart/form-data" class="registration-form">
+            <div class="sign-up-input-container">
+              <div class="sign-up-left">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required>
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="phone">Phone Number:</label>
+                <input type="tel" id="phone" name="phone" required>
+
+                <label for="birthday">Birthday:</label>
+                <input type="date" id="birthday" name="birthday" required>
+
+                <label for="gender">Gender:</label>
+                  <select id="gender" name="gender" required>
+                     <option value="male">Male</option>
+                     <option value="female">Female</option>
+                     <option value="other">Other</option>
+                  </select>
+             
+              
+                <label for="flatNumber">Flat Number:</label>
+                <input type="text" id="flatNumber" name="flatNumber" required>
+
+                <label for="doorNumber">Door Number:</label>
+                <input type="text" id="doorNumber" name="doorNumber" required>
+
+                <label for="nic">NIC:</label>
+                <input type="text" id="nic" name="nic" required>
+
+                <div class="file-upload-wrapper">
+                   <label for="nicPhoto">Photo of NIC:</label>
+                   <button type="button" onclick="document.getElementById('nicPhoto').click()" class="custom-file-upload">Choose File</button>
+                   <span id="nicPhotoName">No file chosen</span>
+                   <input type="file" id="nicPhoto" name="nicPhoto" accept="image/*" required style="display: none;">
+                </div>
+
+                <div class="file-upload-wrapper">
+                   <label for="agreementPhoto">Photo of Agreement Document:</label>
+                   <button type="button" onclick="document.getElementById('agreementPhoto').click()" class="custom-file-upload">Choose File</button>
+                   <span id="agreementPhotoName">No file chosen</span>
+                   <input type="file" id="agreementPhoto" name="agreementPhoto" accept="image/*" required style="display: none;">
+                </div>
+             
+              <div class="sign-up-bottom">
+                 <label for="password">Enter Password:</label>
+                 <input type="password" id="password" name="password" required>
+       
+                <label for="confirmPassword">Confirm Password:</label>
+                <input type="password" id="confirmPassword" name="confirmPassword" required>
+       
+                <label>
+                <input type="checkbox" name="terms" required>I agree to the Terms and Conditions
+               </label>
+
+                <button type="submit">Submit</button>
+              </div>
+            </div>
+          </div>
+     </form>
+    </div>
