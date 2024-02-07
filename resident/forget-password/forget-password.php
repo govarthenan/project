282937813e36857650050a76aa066cc699359e7e
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-07 11:24:34 +0530

    Styled the forget password page.

--- resident/forget-password/forget-password.php
+++ resident/forget-password/forget-password.php
@@ -9,25 +9,29 @@
   </head>
   <body>
     <div class="main-container-forget-password">
-    <div class="forget-password-container">
-        <div class="forget-password-image-container">
-          <img src="Questions-pana.svg" class="forget-password-image">
-          <div class="forget-password-title">Forget password ?</div>
-           <div class="forget-password-form">
-            <form action="/submit-forgot-password" method="post">
-              <div class="form-group">
+      <div class="forget-password-container">
+        <div class="forget-password-container-a">
+           <div class="forget-password-image-container">
+               <img src="Questions-pana.svg" class="forget-password-image">
+           </div>
+           <div class="forget-password-title-container">
+              <div class="forget-password-title">Forget password ?</div>
+           </div>
+        </div>
+        <div class="forget-password-container-b">
+          <form action="/submit-forgot-password" method="post">
+            <div class="form-forget-password">
                 <label for="newpassword">Enter a new password:</label>
                 <input type="text" id="username" name="username" required>
               </div>
-              <div class="form-group">
+            <div class="form-forget-password">
                 <label for="password">Re-type new password:</label>
                 <input type="password" id="password" name="password" required>
-              </div>
-              <button type="save" class="btn-save">Save</button>
-            </form>
-          </div>
+            </div>
+            <button type="save" class="btn-save-forget-password">Save</button>
+          </form>
         </div>
-       
-</div>
+      </div>
+    </div>
 </body>
 </html>
