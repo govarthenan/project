c7cbfaf7acc4715735764c23c9056eaeca604982
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-12 12:23:55 +0530

    Created the 404 error page.

---
+++ 404_error_page/404-error-view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="main-container-404">
+      <img src="404-image.svg" class="img-404">
+      <div class="page-not-found-heading">
+        Page not found!
+      </div>
+      <div class="page-not-found-description">
+        We're sorry,the page you requested<br> could not be found.please go back<br> to the homepage!
+      </div>
+      <div class="home-button">Home Page</div>
+   </div>
+</body>
+</html>
